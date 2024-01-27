d5d03e0154c7603e0bdc746328fc88a937a265f9
Marcio Vale <mvale@smartdigit.pt>
2024-01-27 14:56:19 +0000

    Create a structure for pizzashop app with next

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import './globals.css'
+
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+export const metadata: Metadata = {
+  title: 'Pizza Shop',
+  description: 'This is a delivery app created with Next.js and TypeScript.',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Send</Button>
+    </div>
+  )
+}
