cf94adb3f85b7ee34f48ea1afbe625c803fbb912
Marcio Vale <mvale@smartdigit.pt>
2024-01-30 17:58:45 +0000

    Create orders components

---
+++ src/components/dashboard/orders/order-table-row.tsx
@@ -0,0 +1,41 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Order details</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        234rwerdfsdt34534534
+      </TableCell>
+      <TableCell className="text-muted-foreground">at 15 minutes</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pending</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Marcio Vale</TableCell>
+      <TableCell className="font-medium">€29.99</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Approve
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancel
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
--- src/components/dashboard/orders/orders.tsx
+++ src/components/dashboard/orders/orders.tsx
@@ -1,3 +1,45 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
+
 export function Orders() {
-  return <p>Orders Page</p>
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Orders</h1>
+      </div>
+      <div className="space-y-2.5">
+        <OrderTableFilters />
+
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identifier</TableHead>
+                <TableHead className="w-[180px]">Accomplished At</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Customer</TableHead>
+                <TableHead className="w-[140px]">Amount</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, index) => {
+                return <OrderTableRow key={String(index)} />
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </>
+  )
 }
