f357abfd6223cdd8d54f7421955fe824a39169a4
Marcio Vale <mvale@smartdigit.pt>
2024-02-01 23:42:44 +0000

    feat: Setup the suppabase
    Trying setup suppabase and authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,20 @@
 // import { authenticate } from 'auth-provider'
 import { NextRequest, NextResponse } from 'next/server'
 
+import { authenticate } from './lib/authenticate'
+
 export function middleware(request: NextRequest) {
-  // const isAuthenticated = authenticate(request)
+  const isAuthenticated = authenticate(request)
 
   // If the user is authenticated, continue as normal
-  // if (isAuthenticated) {
-  // return NextResponse.redirect(new URL('/dashboard', request.url))
-  // }
-  return NextResponse.next()
+  if (isAuthenticated) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
 
   // Redirect to login page if not authenticated
-  // return NextResponse.redirect(new URL('/signin', request.url))
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/', '/dashboard/:path*'],
+  matcher: ['/signin', '/dashboard/:path*'],
 }
